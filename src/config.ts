7ba567415797d139c0354ff2fbebce97915db5d5
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2022-10-11 01:04:22 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export let USERS_SEARCH_API_URL: string
 
 export const setConfigVariables = (name?: string) => {
   USERS_API_URL = name ? `https://users.${name}/api/v2` : 'https://users.api.feeds.dev/api/v2'
-  POSTS_API_URL = name ? `https://posts.${name}/api/v3` : 'https://posts.api.feeds.dev/api/v3'
-  FEED_API_URL = name ? `https://posts.${name}/api/v3` : 'https://posts.api.feeds.dev/api/v3'
+  POSTS_API_URL = name ? `https://posts.${name}/api/v4` : 'https://posts.api.feeds.dev/api/v4'
+  FEED_API_URL = name ? `https://posts.${name}/api/v4` : 'https://posts.api.feeds.dev/api/v4'
   USERS_SEARCH_API_URL = name ? `https://users.${name}/api/v3` : 'https://users.api.feeds.dev/api/v3'
 }
